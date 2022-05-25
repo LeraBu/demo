1878e9357dc4253bb6a263d1d1da92280ea99f6b
Lera Burtseva <burtsevaleria@gmail.com>
2022-05-25 14:57:33 +0300

    New commit 25.05.2022

--- src/main/java/com/example/demo/ControllerDemo.java
+++ src/main/java/com/example/demo/ControllerDemo.java
@@ -3,14 +3,23 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class ControllerDemo {
 
-    @GetMapping("/result")
+    @GetMapping("/hello")
+    public String getNewRoud(HttpServletRequest request){
+        String newRoute = request.getParameter("newRoute");
+        System.out.println("Route is " + newRoute);
+        return Task.isItAGoodRoute(newRoute);
+    }
+
+    /* @GetMapping("/result")
     public String result(){
         String route = "NSEWNSWENSNNNNNNNN";
         String goodOrBad = Task.isItAGoodRoute(route);
         return goodOrBad;
-    }
+    } */
 }
 
