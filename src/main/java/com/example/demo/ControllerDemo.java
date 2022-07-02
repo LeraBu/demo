b1c1ed5ffd84b5b64b1abf105125fa0d3b892389
Lera Burtseva <burtsevaleria@gmail.com>
2022-07-02 19:45:58 +0300

    New commit 25.05.2022

--- src/main/java/com/example/demo/ControllerDemo.java
+++ src/main/java/com/example/demo/ControllerDemo.java
@@ -4,16 +4,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static com.example.demo.DemoConnection.connection;
+import static com.example.demo.DemoConnection.statement;
 
 @RestController
 public class ControllerDemo {
 
     @GetMapping("/hello")
-    public String getNewRoud(HttpServletRequest request){
+    public String getNewRoud(HttpServletRequest request) throws SQLException {
         String newRoute = request.getParameter("newRoute");
         System.out.println("Route is " + newRoute);
+        System.out.println(Task.isItAGoodRoute(newRoute));
+
+       String SQL = "INSERT INTO RoutesOne VALUES(" + newRoute + "); commit;";
+      //  ResultSet resultSet = statement.executeQuery(SQL);
+     //    while (resultSet.next()){
+          statement.executeUpdate(SQL);
+      //  }
+
         return Task.isItAGoodRoute(newRoute);
     }
+
+
+
+  /*  public ArrayList<Route> index(){
+        ArrayList<Route> routes = new ArrayList<Route>();
+        String SQL = "SELECT * FROM routes";
+        ResultSet resultSet = DemoConnection.statement.executeQuery(SQL);
+
+        while (resultSet.next()){
+            Route route = new Route(resultSet.getInt("id"), resultSet.getString("description"));
+          //  route.setId();
+          //  route.setDescription();
+
+            routes.add(route);
+        }
+
+            return routes;
+    } */
 
     /* @GetMapping("/result")
     public String result(){
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,22 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static com.example.demo.DemoConnection.statement;
+
 @SpringBootApplication
 public class DemoApplication {
+
+	public DemoApplication() {
+	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
