288a3af2d60bc16b7a262d12cc14c65a29f9415d
Lera Burtseva <burtsevaleria@gmail.com>
2022-08-28 10:20:00 +0300

    New commit 28.08.2022 with tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,45 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class DemoApplicationTests {
+//@ContextConfiguration(classes = {SpringConfig.class})
+//@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class DemoApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ControllerDemo controllerDemo;
+
+    @Test
+    public void test() throws Exception {
+        assertThat(controllerDemo).isNotNull();
+    }
+
+    @Test
+    public void helloTest() throws Exception {
+        String route = "NSNSNSNSNS";
+        mockMvc.perform(get("/hello?newRoute={route}", route))
+                .andExpect(content().string("This is a good route"));
+    }
 
 }
 
